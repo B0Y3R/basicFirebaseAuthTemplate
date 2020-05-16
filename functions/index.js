00526102d1dfcf1772304d266ed1e7bd633f0664
James Boyer <jamesboyer92@user.noreply.github.com>
2020-05-15 23:03:17 -0400

    login api created

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,17 @@ const {
     postOneTodo,
 } = require('./api/todos');
 
+const {
+    loginUser
+} = require('./api/users')
+
+//Todos
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.put('/todo/:todoId', editTodo);
 app.delete('/todo/:todoId', deleteTodo);
+
+//Users
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
